339ae05dd3b1b4ba2c6ceaa9eb1baa6f51c07a32
“nimajkrap” <jamin2205@gmail.comgit config --global user.email jamin2205@gmail.com>
2024-11-18 12:11:26 +0900

    11/18

--- app.py
+++ app.py
@@ -38,7 +38,9 @@ def method():
     else:
         keyword = request.form["keyword"]
         print(keyword)
-        return f"POST로 전달된 당신이 입력한 검색어: {keyword}"
+        data = nvapi.blog(keyword)
+        # return f"POST로 전달된 당신이 입력한 검색어: {keyword}"
+        return render_template("result.html", keyword=keyword, blist=data)
 
 if __name__ == '__main__':
     app.run(host="210.110.167.51")
--- test.py
+++ test.py
@@ -6,7 +6,7 @@ nvapi.blog(keyword)
 
 # 밑에는 연습
 def hello(msg):
-    #print(f"나는 귀염둥이 {msg}")
+    #print(f"겁나게 맛있는 {msg}")
     #return "출력" # 결과 값(있어도 그만 없어도 그만)
     return f"둘이 먹다 한명이 죽어도 모를 {msg}"
 
@@ -16,3 +16,13 @@ def hello(msg):
 
     #result = hello("keyword") # 위 함수에서 전달된 값을 result에 전달
     #print(result) # 전달된 값을 화면에 출력
+
+def hello2():
+    menu = ["짜장면", "김밥", "라면", " 돈까스"] # 리스트
+    dmenu = {"메뉴":"짜장면", "가격": 5000} # 딕셔너리
+    return menu
+
+# menu = hello2()
+# print(menu)
+# for m in menu:
+#     print(m)
